bee39a6802dcf9a728fc309cda7b176d620a17a8
Chris Yates <chris@clyates.plus.com>
2024-01-30 15:43:49 +0000

    RulesMiddleware.php to register RBAC rules

---
+++ config/di/rules.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @copyright Copyright © 2024 BeastBytes - All rights reserved
+ * @license BSD 3-Clause
+ */
+
+declare(strict_types=1);
+
+/** @var array $params */
+
+use BeastBytes\Yii\Rbam\RulesMiddleware;
+
+return [
+    RulesMiddleware::class => [
+        'class' => RulesMiddleware::class,
+        'rules()' => [
+            'rules' => array_keys($params['yiisoft/rbac-rules-container']['rules']),
+        ],
+    ]
+];
