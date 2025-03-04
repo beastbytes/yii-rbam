5a6f343846c8fe41d030f8d3fe14081c76409a0d
Chris <chris@clyates.plus.com>
2025-03-04 11:42:23 +0000

    Update type for name

--- src/Command/Attribute/Permission.php
+++ src/Command/Attribute/Permission.php
@@ -5,14 +5,13 @@ declare(strict_types=1);
 namespace BeastBytes\Yii\Rbam\Command\Attribute;
 
 use Attribute;
-use BeastBytes\Yii\Rbam\Permission as RbamPermission;
 use StringBackedEnum;
 
 #[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 final class Permission
 {
     public function __construct(
-        private RbamPermission|string $name,
+        private StringBackedEnum|string $name,
         private string $description,
         private StringBackedEnum|string $parent,
         private string $ruleName,
