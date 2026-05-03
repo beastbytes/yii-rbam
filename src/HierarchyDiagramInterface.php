2d1c13c9c1be7923d4d520fb447a031f993fc1c7
chris <chris@clyates.plus.com>
2026-05-03 14:54:25 +0100

    Add withItem() method Improve rendering

--- src/HierarchyDiagramInterface.php
+++ src/HierarchyDiagramInterface.php
@@ -4,20 +4,11 @@ declare(strict_types=1);
 
 namespace BeastBytes\Yii\Rbam;
 
+use Stringable;
 use Yiisoft\Rbac\Item;
-use Yiisoft\Rbac\ItemsStorageInterface;
-use Yiisoft\Router\UrlGeneratorInterface;
-use Yiisoft\Strings\Inflector;
-use Yiisoft\Translator\TranslatorInterface;
 
-Interface HierarchyDiagramInterface extends \Stringable
+Interface HierarchyDiagramInterface extends Stringable
 {
-    public function __construct(
-        Item $item,
-        ItemsStorageInterface $itemsStorage,
-        Inflector $inflector,
-        TranslatorInterface $translator,
-        UrlGeneratorInterface $urlGenerator
-    );
     public function render(): string;
+    public function withItem(Item $item): self;
 }
--- src/MermaidHierarchyDiagram.php
+++ src/MermaidHierarchyDiagram.php
@@ -11,6 +11,8 @@ use BeastBytes\Mermaid\ClassDiagram\Method;
 use BeastBytes\Mermaid\ClassDiagram\Relationship;
 use BeastBytes\Mermaid\ClassDiagram\RelationshipType;
 use BeastBytes\Mermaid\InteractionType;
+use BeastBytes\Mermaid\Mermaid;
+use RuntimeException;
 use Yiisoft\Rbac\Item;
 use Yiisoft\Rbac\ItemsStorageInterface;
 use Yiisoft\Router\UrlGeneratorInterface;
@@ -20,16 +22,22 @@ use Yiisoft\Translator\TranslatorInterface;
 final class MermaidHierarchyDiagram implements HierarchyDiagramInterface
 {
     private array $classes = [];
+    private ?Item $item = null;
     private array $relationships = [];
 
     public function __construct(
-        private readonly Item $item,
         private readonly ItemsStorageInterface $itemsStorage,
-        private readonly Inflector $inflector,
         private readonly TranslatorInterface $translator,
         private readonly UrlGeneratorInterface $urlGenerator,
     )
     {
+    }
+
+    public function withItem(Item $item): self
+    {
+        $new = clone $this;
+        $new->item = $item;
+        return $new;
     }
 
     public function __toString(): string
@@ -39,93 +47,104 @@ final class MermaidHierarchyDiagram implements HierarchyDiagramInterface
 
     public function render(): string
     {
-        $ancestors = $this
-            ->itemsStorage
-            ->getParents($this->item->getName())
-        ;
-
         $currentItem = (new Classs(
             $this->item->getName(),
-            $this->translator->translate('label.' . $this->item->getType())
+            $this->translator->translate('label.' . ItemTypeService::getItemType($this->item))
         ))
-            ->withMember(new Attribute($this->item->getDescription()))
-            ->withStyleClass('current_' . $this->item->getType())
+            ->withAttribute(new Attribute($this->item->getDescription()))
+            ->withStyleClass('current_' . ItemTypeService::getItemType($this->item))
         ;
-        $this->classes[$this->item->getName()] = $this->item->getRuleName()
-            ? $currentItem->addMember(new Method($this->item->getRuleName()))
+        $this->classes[$this->item->getName()] = is_string($this->item->getRuleName())
+            ? $currentItem->withMethod(new Method($this->item->getRuleName()))
             : $currentItem
         ;
 
-        $child = $currentItem;
+        $this->ancestors($currentItem);
+        $this->descendants($currentItem);
+        $this->relationships();
 
-        foreach ($ancestors as $ancestor) {
-            $parent = (new Classs(
-                $ancestor->getName(),
-                $this->translator->translate('label.' . $ancestor->getType())
-            ))
-                ->withMember(new Attribute($ancestor->getDescription()))
-                ->withStyleClass('ancestor_' . $ancestor->getType())
-                ->withInteraction(
-                    $this->urlGenerator->generate(
-                        'rbam.viewItem',
-                        [
-                            'type' => $ancestor->getType(),
-                            'name' => $this->inflector->toSnakeCase($ancestor->getName())
-                        ]
-                    ),
-                    InteractionType::Link
-                )
-            ;
-
-            if (!array_key_exists($ancestor->getName(), $this->classes)) {
-                $this->classes[$ancestor->getName()] = $ancestor->getRuleName()
-                    ? $parent->addMember(new Method($ancestor->getRuleName()))
-                    : $parent
-                ;
-            }
-
-            $this->relationships[] = new Relationship($parent, $child, RelationshipType::Inheritance);
-            $child = $parent;
-        }
-
-        $this->getDescendants($currentItem);
-
-        return (new ClassDiagram())
+        $return = Mermaid::create(ClassDiagram::class)
             ->withClass(...array_values($this->classes))
             ->withRelationship(...$this->relationships)
             ->render(['id' => 'mermaid'])
         ;
+
+        return str_replace('<<', '&lt;&lt;', $return);
     }
 
-    private function getDescendants(Classs $item): void
+    private function ancestors(Classs $child): void
     {
-        foreach ($this->itemsStorage->getDirectChildren($item->getId()) as $child) {
-            $childClass = (new Classs(
-                $child->getName(),
-                $this->translator->translate('label.' . $child->getType())
+        $childName = $child->getId();
+        $parentItems = $this->itemsStorage->getParents($child->getId());
+
+        foreach ($parentItems as $parentItem) {
+            $parent = (new Classs(
+                $parentItem->getName(),
+                $this->translator->translate('label.' . $parentItem->getType())
             ))
-                ->withMember(new Attribute($child->getDescription()))
-                ->withStyleClass('descendant_' . $child->getType())
+                ->withAttribute(new Attribute($parentItem->getDescription()))
+                ->withStyleClass('ancestor_' . $parentItem->getType())
                 ->withInteraction(
                     $this->urlGenerator->generate(
-                        'rbam.viewItem',
+                        'rbam.item.view',
                         [
-                            'type' => $child->getType(),
-                            'name' => $this->inflector->toSnakeCase($child->getName())
+                            'type' => $parentItem->getType(),
+                            'name' => $parentItem->getName()
                         ]
                     ),
-                    InteractionType::Link
+                    InteractionType::link
                 )
             ;
 
-            if (!array_key_exists($child->getName(), $this->classes)) {
-                $this->classes[$child->getName()] = $child->getRuleName()
-                    ? $childClass->addMember(new Method($child->getRuleName()))
-                    : $childClass
+            if (!array_key_exists($parentItem->getName(), $this->classes)) {
+                $this->classes[$parentItem->getName()] = $parentItem->getRuleName()
+                    ? $parent->addMethod(new Method($parentItem->getRuleName()))
+                    : $parent
                 ;
             }
-            $this->relationships[] = new Relationship($item, $childClass, RelationshipType::Inheritance);
-            $this->getDescendants($childClass);
+        }
+    }
+
+    private function descendants(Classs $parent): void
+    {
+        foreach ($this->itemsStorage->getDirectChildren($parent->getId()) as $item) {
+            $child = (new Classs(
+                $item->getName(),
+                $this->translator->translate('label.' . ItemTypeService::getItemType($item))
+            ))
+                ->withAttribute(new Attribute($item->getDescription()))
+                ->withStyleClass('descendant_' . ItemTypeService::getItemType($item))
+                ->withInteraction(
+                    $this->urlGenerator->generate(
+                        'rbam.item.view',
+                        [
+                            'type' => ItemTypeService::getItemType($item),
+                            'name' => $item->getName()
+                        ]
+                    ),
+                    InteractionType::link
+                )
+            ;
+
+            if (!array_key_exists($item->getName(), $this->classes)) {
+                $this->classes[$item->getName()] = is_string($item->getRuleName())
+                    ? $child->addMethod(new Method($item->getRuleName()))
+                    : $child
+                ;
+            }
+
+            $this->descendants($child);
+        }
+    }
+
+    private function relationships(): void
+    {
+        foreach ($this->classes as $parent) {
+            foreach ($this->classes as $child) {
+                if ($this->itemsStorage->hasDirectChild($parent->getId(), $child->getId())) {
+                    $this->relationships[] = new Relationship($parent, $child, RelationshipType::inheritance);
+                }
+            }
         }
     }
 }
