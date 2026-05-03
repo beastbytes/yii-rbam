31c39bfa7a798fef1aa24e7cb5023bb1c4de5bba
chris <chris@clyates.plus.com>
2026-05-03 14:56:47 +0100

    Add/update assets

--- src/Assets/PaginationAsset.php
+++ src/Assets/PaginationAsset.php
@@ -1,25 +1,18 @@
 <?php
-/**
- * @copyright Copyright © 2025 BeastBytes - All rights reserved
- * @license BSD 3-Clause
- */
 
 declare(strict_types=1);
 
 namespace BeastBytes\Yii\Rbam\Assets;
 
-use BeastBytes\Yii\Widgets\Assets\DialogAsset;
 use Yiisoft\Assets\AssetBundle;
 
-class RemoveAsset extends AssetBundle
+class PaginationAsset extends AssetBundle
 {
     public ?string $basePath = '@assets';
     public ?string $baseUrl = '@assetsUrl';
     public array $js = [
-        'js/remove.js',
+        'js/paginator.js',
     ];
     public ?string $sourcePath = '@assetsSource';
-    public array $depends = [
-        DialogAsset::class
-    ];
+    //public ?string $sourcePath = __DIR__ . '/../resources/assets';
 }
