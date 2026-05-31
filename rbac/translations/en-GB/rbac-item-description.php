b2669112991681c098a4113f016a5cf12408630e
chris <chris@clyates.plus.com>
2026-05-31 14:36:51 +0100

    Update translation messages

---
+++ rbac/translations/en-GB/rbac-item-description.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'rbam.admin' => 'Perform all RBAM functions',
+    'rbam.clear' => 'Clear RBAC Assignments, Permissions, and Roles',
+    'rbam.index' => 'View RBAM',
+    'rbam.item.create' => 'Create Permissions and Roles',
+    'rbam.item.manager' => 'Create, update, delete, and view RBAC Roles and Permissions',
+    'rbam.item.remove' => 'Remove Permissions and Roles',
+    'rbam.item.update' => 'Update Permissions and Roles',
+    'rbam.item.view' => 'View Permissions and Roles',
+    'rbam.rule.create' => 'Create Rules',
+    'rbam.rule.delete' => 'Delete Rules',
+    'rbam.rule.manager' => 'Create, update, delete, and view RBAC Rules',
+    'rbam.rule.update' => 'Update Rules',
+    'rbam.rule.view' => 'View Rules',
+    'rbam.user.manager' => 'View users, and assign, and revoke RBAC Roles',
+    'rbam.user.update' => 'Update Users',
+    'rbam.user.view' => 'View Users',
+];
