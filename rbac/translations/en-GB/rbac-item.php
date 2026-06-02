25616a5c163479b84ac26347e525ff40525af995
chris <chris@clyates.plus.com>
2026-06-02 19:41:14 +0100

    Rename files

---
+++ rbac/translations/en-GB/rbac-item.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'rbam.administrator' => 'RBAM Administrator',
+    'rbam.administrator.description' => 'Perform all RBAM functions',
+    'rbam.clear.description' => 'Clear RBAC Assignments, Permissions, and Roles',
+    'rbam.index.description' => 'View RBAM',
+    'rbam.item.create.description' => 'Create Permissions and Roles',
+    'rbam.item.manager.description' => 'Create, update, delete, and view RBAC Roles and Permissions',
+    'rbam.item.remove.description' => 'Remove Permissions and Roles',
+    'rbam.item.update.description' => 'Update Permissions and Roles',
+    'rbam.item.view.description' => 'View Permissions and Roles',
+    'rbam.rule.create.description' => 'Create Rules',
+    'rbam.rule.delete.description' => 'Delete Rules',
+    'rbam.rule.manager.description' => 'Create, update, delete, and view RBAC Rules',
+    'rbam.rule.update.description' => 'Update Rules',
+    'rbam.rule.view.description' => 'View Rules',
+    'rbam.user.manager.description' => 'View users, and assign, and revoke RBAC Roles',
+    'rbam.user.update.description' => 'Update Users',
+    'rbam.user.view.description' => 'View Users',
+];
---
+++ rbac/translations/en-GB/rbac-rule.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'Always returns FALSE' => 'Always returns FALSE',
+    'Always returns TRUE' => 'Always returns TRUE',
+];
