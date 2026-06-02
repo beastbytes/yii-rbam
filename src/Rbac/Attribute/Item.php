78234f87c1ae3520029d5e9249eed6b8d8fe1761
chris <chris@clyates.plus.com>
2026-06-02 19:37:24 +0100

    Change $name to $item

--- src/Rbac/Attribute/Item.php
+++ src/Rbac/Attribute/Item.php
@@ -9,28 +9,28 @@ use BeastBytes\Yii\Rbam\Rbac\ItemInterface;
 abstract class Item
 {
     /**
-     * @param ItemInterface $name
-     * @param string $description
+     * @param ItemInterface $item
+     * @param ?string $description
      * @param ItemInterface|list<ItemInterface> $parent
      * @param string|null $ruleName
      */
     public function __construct(
-        private readonly ItemInterface $name,
-        private readonly string $description = '',
+        private readonly ItemInterface $item,
+        private readonly ?string $description = null,
         private readonly array|ItemInterface $parent = [],
         private readonly ?string $ruleName = null,
     )
     {
     }
 
-public function getDescription(): string
+    public function getDescription(): string
     {
-        return $this->description;
+        return $this->description ?? '';
     }
 
     public function getName(): string
     {
-        return $this->name->getItemName();
+        return $this->item->getItemName();
     }
 
     public function getParents(): array
